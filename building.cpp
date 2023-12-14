0adb9ff35ca47beb91371f0a9c168b06c4b9f14f
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-14 09:49:59 -0800

    Converted from Maze to SkipList in building.cpp

--- building.cpp
+++ building.cpp
@@ -2,30 +2,16 @@
 #include "applib/random.h"
 
 #include <iostream>
+#include <sstream>
 
 
 int main()
 {
-    Maze myMaze;
-
-    //don't need to loop through tests. That coverage is being done by gTest.
-    //can loop through one tho...
-
-    string mazeFile = "data/maze0.txt";
-    if (!myMaze.load(mazeFile))
-    {
-        cerr << "Failed to load " << mazeFile << endl;
-        return -1;
-    }
-    cout << "*** Solving " << mazeFile << endl;
-    if (myMaze.solve())
-    {
-        cout << "Path: " << myMaze.getPath() << endl;
-    }
-    else
-    {
-        cout << "Failed to solve: " << mazeFile << endl;
-    }
-    cout << myMaze << endl;
+    stringstream outSS;
+    SkipList skp;
+    skp.add(3);
+    outSS << skp;
+    cout << outSS.str() << endl;
+    //assert(outSS.str() == "[level: 1] 3-->nullptr\n");
 }
 
