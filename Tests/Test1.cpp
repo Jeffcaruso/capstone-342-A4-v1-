3ad1c1405da047d91a18e8c0f3e2c6bea03cc70c
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-12-14 11:00:11 -0800

    Removed un-needed test 5. Changed test 1. it should not be at least close to where it needs to be

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -6,7 +6,8 @@
 //============================================================================
 
 #include <gtest/gtest.h>
-#include "applib/maze.h"
+#include "applib/skiplist.h"
+#include "applib/random.h"
 
 #include <iostream>
 #include <sstream>
@@ -15,36 +16,38 @@ using namespace std;
 
 TEST(Test1, maze0)
 {
-	Maze myMaze;
-    string mazeFile = "data/maze0.txt";
-    bool result = false;
-    result = myMaze.load(mazeFile);
-    if (! result)
-    {
-        cerr << "Failed to load " << mazeFile << endl;
-        //rest of this is useless without the test file being loaded in, so exit here...
-        ASSERT_TRUE(result); 
-        return;
-    }
-    //if made it here, should pass.
-    EXPECT_TRUE(result); 
 
-    cout << "*** Solving " << mazeFile << endl;
-    result = myMaze.solve();
-    if (result)
-    {
-        EXPECT_TRUE(result);
-        cout << "Path: " << myMaze.getPath() << endl;
-    }
-    else
-    {
-        //should fail here... b/c solve() failed to solve it.
-        EXPECT_TRUE(result);
-        cout << "Failed to solve: " << mazeFile << endl;
-    }
-    cout << myMaze << endl;
-    string expectedPath;
-    //solving order of: N E S W... (with backtracking)
-    expectedPath = "NNNNNESSSSENNNNESSSSENNNNESSSSENNNNN";
-    EXPECT_EQ(myMaze.getPath(), expectedPath);
+
+	// Maze myMaze;
+    // string mazeFile = "data/maze0.txt";
+    // bool result = false;
+    // result = myMaze.load(mazeFile);
+    // if (! result)
+    // {
+    //     cerr << "Failed to load " << mazeFile << endl;
+    //     //rest of this is useless without the test file being loaded in, so exit here...
+    //     ASSERT_TRUE(result); 
+    //     return;
+    // }
+    // //if made it here, should pass.
+    // EXPECT_TRUE(result); 
+
+    // cout << "*** Solving " << mazeFile << endl;
+    // result = myMaze.solve();
+    // if (result)
+    // {
+    //     EXPECT_TRUE(result);
+    //     cout << "Path: " << myMaze.getPath() << endl;
+    // }
+    // else
+    // {
+    //     //should fail here... b/c solve() failed to solve it.
+    //     EXPECT_TRUE(result);
+    //     cout << "Failed to solve: " << mazeFile << endl;
+    // }
+    // cout << myMaze << endl;
+    // string expectedPath;
+    // //solving order of: N E S W... (with backtracking)
+    // expectedPath = "NNNNNESSSSENNNNESSSSENNNNESSSSENNNNN";
+    // EXPECT_EQ(myMaze.getPath(), expectedPath);
 }
