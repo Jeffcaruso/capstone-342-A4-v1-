1a5adebe07c48d5cbbd670056e667e6dbca0e714
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2024-02-22 03:30:39 +0000

    Initial Commit. Test 0 should be working.

---
+++ Test0.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Test1
+// Test Desc.  : Trivial testing - largely to just slightly support build validation
+// Author      : Jeffrey Caruso
+// Date    	   : Fall 2023
+//============================================================================
+
+#include <gtest/gtest.h>
+#include "skiplist.h"
+#include "random.h"
+
+//using namespace std;
+
+namespace {
+    TEST(Test0, TrivialTest)
+    {
+        //Make LargeNum class ln
+        //test the build
+        SkipList skplst();
+
+        //always true
+        EXPECT_EQ(0, 0);
+    }
+
+} // namespace
+
